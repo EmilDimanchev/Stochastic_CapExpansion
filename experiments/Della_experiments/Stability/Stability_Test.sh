c39912f206ca3141e0e618b744288307534c873a
Mike Lau <ml6802@princeton.edu>
2026-04-13 17:15:19 -0700

    should be empty

--- experiments/Della_experiments/Stability/Stability_Test.sh
+++ experiments/Della_experiments/Stability/Stability_Test.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                           # node count
 #SBATCH --ntasks=1                          # total number of tasks across all nodes
 #SBATCH --cpus-per-task=32                   # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=3G                    # memory per cpu-core
+#SBATCH --mem-per-cpu=2G                    # memory per cpu-core
 #SBATCH --time=2:00:00                     # total run time limit (HH:MM:SS)
 #SBATCH --output="test.out"
 #SBATCH --mail-type=end                    # notifications for job done & fail
